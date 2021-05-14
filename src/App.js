da495d4a7ef34a018bc2e24521995ee819a4d735
JR <naveenreddysnkolar@gmail.com>
2021-05-14 11:19:34 +0530

    login code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ValidatedLoginForm from "./ValidatedLoginForm";
+import "./style.css";
+
+function App() {
+    return(
+        <div className="App">
+            <h1>Validated Login Form</h1>
+            <ValidatedLoginForm />
+        </div>
+    );
+}
+export default App;
