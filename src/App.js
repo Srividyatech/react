ffb1b52ccd63b44436733bec2580c2bfd41a54e0
JR <naveenreddysnkolar@gmail.com>
2021-05-22 13:36:10 +0530

    restaurant app creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import "./style.css";
-import ValidatedLoginForm from './ValidatedLoginForm';
 
 function App() {
-    return(
-        <div className="App">
-            <h1>Validated Login Form</h1>
-            <ValidatedLoginForm />
-        </div>
+    return (
+        <h1>React App</h1>
     );
 }
 export default App;
